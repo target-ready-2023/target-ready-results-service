99cd6bdfb7c7d1926ad8214f54b5de6287249a7e
PoojaRemesh <itspooja1607@gmail.com>
2023-07-16 18:19:57 +0530

    schedule-post-initial

--- src/main/java/com/target/targetreadyresultsservice/controller/ScheduleController.java
+++ src/main/java/com/target/targetreadyresultsservice/controller/ScheduleController.java
@@ -1,9 +1,30 @@
 package com.target.targetreadyresultsservice.controller;
 
+import com.target.targetreadyresultsservice.model.Schedule;
+import com.target.targetreadyresultsservice.service.ScheduleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/schedule/v1")
 public class ScheduleController {
+    private final ScheduleService scheduleService;
+
+    public ScheduleController(ScheduleService scheduleService) {
+        this.scheduleService = scheduleService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addNewSchedule(@RequestBody Schedule schedule){
+        try{
+            scheduleService.addNewScheduleTest(schedule);
+            return new ResponseEntity<>("Successful", HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.EXPECTATION_FAILED);
+        }
+    }
 }
--- src/main/java/com/target/targetreadyresultsservice/service/ScheduleService.java
+++ src/main/java/com/target/targetreadyresultsservice/service/ScheduleService.java
@@ -1,8 +1,11 @@
 package com.target.targetreadyresultsservice.service;
 
+import com.target.targetreadyresultsservice.model.Schedule;
 import com.target.targetreadyresultsservice.repository.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ScheduleService {
@@ -11,5 +14,64 @@ public class ScheduleService {
     @Autowired
     public ScheduleService(ScheduleRepository scheduleRepository) {
         this.scheduleRepository = scheduleRepository;
+    }
+
+    public void addNewScheduleTest(Schedule schedule){
+        long num = scheduleRepository.count();
+        if(num==0){
+            if(schedule.getScheduleType().contains("Test")){
+                schedule.setScheduleCode("T1");
+            }
+            else{
+                schedule.setScheduleCode("E1");
+            }
+        }
+        else{
+            if(schedule.getScheduleType().contains("Test")){
+                schedule.setScheduleCode("T"+num+1);
+            }
+            else{
+                schedule.setScheduleCode("E"+num+1);
+            }
+        }
+
+    }
+
+    public void addNewSchedule(Schedule schedule) {
+        if(schedule.getScheduleType().contains("Test")){
+            schedule.setScheduleCode(setTestCode());
+        }
+        else{
+            schedule.setScheduleCode(setExamCode());
+        }
+        scheduleRepository.save(schedule);
+    }
+
+    private String setExamCode() {
+        List<Schedule> scheduleList = scheduleRepository.findAll();
+        if(scheduleList.isEmpty()){
+            return "E1";
+        }
+        int num=1;
+        for (Schedule s: scheduleList) {
+            if(s.getScheduleType().contains("Exam")){
+                num++;
+            }
+        }
+        return "E"+num;
+    }
+
+    private String setTestCode() {
+        List<Schedule> scheduleList = scheduleRepository.findAll();
+        if(scheduleList.isEmpty()){
+            return "T1";
+        }
+        int num=1;
+        for (Schedule s: scheduleList) {
+            if(s.getScheduleType().contains("Test")){
+                num++;
+            }
+        }
+        return "T"+num;
     }
 }
