6482aa690f341fd9a9d8a7bc8126870d8d32986b
Soumya <gurramsoumyadevender@gmail.com>
2023-07-25 12:20:10 +0530

    subject testcases

--- src/main/java/com/target/targetreadyresultsservice/repository/SubjectRepository.java
+++ src/main/java/com/target/targetreadyresultsservice/repository/SubjectRepository.java
@@ -4,7 +4,9 @@ import com.target.targetreadyresultsservice.model.Subject;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SubjectRepository extends MongoRepository<Subject, String> {
-
+List<Subject>findBySubjectName(String subjectName);
 }
--- src/main/java/com/target/targetreadyresultsservice/service/SubjectService.java
+++ src/main/java/com/target/targetreadyresultsservice/service/SubjectService.java
@@ -2,6 +2,7 @@ package com.target.targetreadyresultsservice.service;
 
 
 import com.target.targetreadyresultsservice.controller.ResultsController;
+import com.target.targetreadyresultsservice.controller.SubjectController;
 import com.target.targetreadyresultsservice.model.Subject;
 import com.target.targetreadyresultsservice.repository.SubjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,7 @@ public class SubjectService {
     }
     public SubjectService() {
     }
-    private static final Logger log = LoggerFactory.getLogger(ResultsController.class);
+    private static final Logger log = LoggerFactory.getLogger(SubjectController.class);
 
     public List<String> getSubjectsGivenClassCode(String code) {
         List<String> subNames = new ArrayList<>();
@@ -44,47 +45,67 @@ public class SubjectService {
         return subNames;
     }
 
-    public  String addSubject(Subject subject) throws Exception {
+    public  Subject addSubject(Subject subject)  {
         String code="S-"+subject.getSubjectName().substring(0,2)+subject.getClassCode();
         subject.setSubjectCode(code);
-        subjectRepository.save(subject);
-        return "Subject added successfully";
+        Optional<Subject> savedSub=subjectRepository.findById(subject.getSubjectCode());
+        if(savedSub.isPresent())
+        {   log.info("Subject already present");
+            throw new RuntimeException("Subject already present");
+        }
+        else {
+            return subjectRepository.save(subject);
+        }
     }
     public List<Subject> getSubjects()
     {
         log.info("fetching all the subjects");
-        return subjectRepository.findAll();
+        List<Subject> subjects=subjectRepository.findAll();
+        if(subjects.isEmpty())
+        {
+            throw new RuntimeException("No subjects found");
+        }
+        return subjects;
     }
 
     public Optional<Subject> getSubjectById(String subjectCode) {
-        return subjectRepository.findById(subjectCode);
+        log.info("getting subject by ID");
+        Optional<Subject> sub=subjectRepository.findById(subjectCode);
+        if(sub.isEmpty())
+        {
+            throw new RuntimeException("No subject found");
+        }
+        return sub;
     }
 
-    public void updateSubject(String subjectCode, Subject subject) {
+    public String updateSubject(String subjectCode, Subject subject) {
         Optional<Subject> subject1= subjectRepository.findById(subjectCode);
         if(subject1.isPresent()) {
+            log.info("updating subject");
             Subject sub = subject1.get();
             sub.setSubjectName(subject.getSubjectName());
             sub.setCredits(subject.getCredits());
             sub.setClassCode(subject.getClassCode());
             subjectRepository.save(sub);
+            return "Updated Successfully";
         }
         else
         {
             log.info("Could not find the subject with code"+subjectCode);
-            throw new RuntimeException();
+            throw new RuntimeException("Could not find the subject with code"+subjectCode);
         }
     }
-    public void deleteSubject(String subjectCode)
+    public String deleteSubject(String subjectCode)
     {
         Optional<Subject> subject=subjectRepository.findById(subjectCode);
         if(subject.isPresent()) {
             log.info("Deleting subject with code "+subjectCode);
             subjectRepository.deleteById(subjectCode);
+            return "Deleted Successfully";
            }
         else {
             log.info("Could not find the subject with code"+subjectCode);
-             throw new RuntimeException();
+             throw new RuntimeException("Could not find the subject with code"+subjectCode);
              }
     }
 
@@ -92,16 +113,16 @@ public class SubjectService {
         subjectRepository.deleteAll();
     }
 
-    public List<Subject> searchSubjectsByFilters(String subjectName)  {
-        Query query = new Query();
-        List<Criteria> criteria = new ArrayList<>();
-        if (subjectName != null && !subjectName.isEmpty())
-            criteria.add(Criteria.where("subjectName").is(subjectName));
-        if (!criteria.isEmpty())
-            query.addCriteria(new Criteria().andOperator(criteria.toArray(new Criteria[criteria.size()])));
-       return mongoTemplate.find(query,Subject.class);
-
-
+    public List<Subject> searchSubjectsByFilters(String subjectName)
+    {
+        log.info("getting subjects with subject name");
+            List<Subject> subjects=subjectRepository.findBySubjectName(subjectName);
+              if(subjects.isEmpty())
+                {
+                    log.info("Could not find any subject with name"+subjectName);
+                    throw new RuntimeException("No subject present with name "+subjectName);
+                }
+           return subjects;
     }
 }
 
