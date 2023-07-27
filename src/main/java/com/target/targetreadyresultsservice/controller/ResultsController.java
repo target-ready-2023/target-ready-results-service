52243539e97f6eb8356550e09e5abd7b60cecad7
aidaAnn <aidaannalex31@gmail.com>
2023-07-27 20:07:17 +0530

    get1 and delete apis added

--- src/main/java/com/target/targetreadyresultsservice/controller/ResultsController.java
+++ src/main/java/com/target/targetreadyresultsservice/controller/ResultsController.java
@@ -1,5 +1,7 @@
 package com.target.targetreadyresultsservice.controller;
 
+import com.target.targetreadyresultsservice.Exception.BlankValueException;
+import com.target.targetreadyresultsservice.Exception.InvalidValueException;
 import com.target.targetreadyresultsservice.Exception.NotFoundException;
 import com.target.targetreadyresultsservice.model.Results;
 import com.target.targetreadyresultsservice.service.ResultsService;
@@ -7,6 +9,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -16,6 +20,28 @@ public class ResultsController {
 
     public ResultsController(ResultsService resultsService) {
         this.resultsService = resultsService;
+    }
+
+    //get class results of an academic year
+    @GetMapping
+    public ResponseEntity<List<Results>> getClassResults(
+            @RequestParam("classCode") String classCode,
+            @RequestParam("academicYear") String acyear
+
+    ){
+        try{
+            List<Results> classResults = resultsService.getClassresult(classCode,acyear);
+            return new ResponseEntity<List<Results>>(classResults,HttpStatus.OK);
+        }
+        catch(NotFoundException e){
+            return new ResponseEntity(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+        catch(InvalidValueException | BlankValueException e){
+            return new ResponseEntity(e.getMessage(),HttpStatus.EXPECTATION_FAILED);
+        }
+        catch(Exception e){
+            return new ResponseEntity("Action Failed",HttpStatus.EXPECTATION_FAILED);
+        }
     }
 
     @PostMapping
@@ -43,4 +69,20 @@ public class ResultsController {
             return new ResponseEntity("Update failed! Please try again",HttpStatus.EXPECTATION_FAILED);
         }
     }
+
+    //delete result
+    @DeleteMapping("/delete/{resultCode}")
+    public ResponseEntity<String> deleleResults(@PathVariable String resultCode){
+        try{
+            resultsService.deleteResult(resultCode);
+            return new ResponseEntity<>("Deleted Sucessfully",HttpStatus.OK);
+        }
+        catch(NotFoundException e){
+            return new ResponseEntity<>("Result does not Exists",HttpStatus.NOT_FOUND);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>("Deletion Failed",HttpStatus.EXPECTATION_FAILED);
+        }
+    }
 }
+
--- src/main/java/com/target/targetreadyresultsservice/service/ResultsService.java
+++ src/main/java/com/target/targetreadyresultsservice/service/ResultsService.java
@@ -1,10 +1,13 @@
 package com.target.targetreadyresultsservice.service;
 
+import com.target.targetreadyresultsservice.Dto.ClassDto;
+import com.target.targetreadyresultsservice.Exception.BlankValueException;
+import com.target.targetreadyresultsservice.Exception.InvalidValueException;
 import com.target.targetreadyresultsservice.Exception.NotFoundException;
 import com.target.targetreadyresultsservice.model.Results;
 import com.target.targetreadyresultsservice.model.Schedule;
-import com.target.targetreadyresultsservice.model.Student;
 import com.target.targetreadyresultsservice.repository.ResultsRepository;
+import com.target.targetreadyresultsservice.model.Student;
 import com.target.targetreadyresultsservice.repository.ScheduleRepository;
 import com.target.targetreadyresultsservice.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,19 +15,61 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.Optional;
+import java.util.List;
 
 @Service
 public class ResultsService{
     private final ResultsRepository resultsRepository;
     private final StudentRepository studentRepository;
     private final ScheduleRepository scheduleRepository;
+    private final ScheduleService scheduleService;
+    private final ClassService classService;
 
     @Autowired
-    public ResultsService(ResultsRepository resultsRepository, StudentRepository studentRepository, ScheduleRepository scheduleRepository) {
+    public ResultsService(ResultsRepository resultsRepository, StudentRepository studentRepository, ScheduleRepository scheduleRepository, ScheduleService scheduleService, ClassService classService) {
         this.resultsRepository = resultsRepository;
         this.studentRepository = studentRepository;
         this.scheduleRepository = scheduleRepository;
+        this.scheduleService = scheduleService;
+        this.classService = classService;
     }
+
+    public List<Results> getClassresult(String className, String acyear) {
+        if (className.isBlank()) {
+            throw new BlankValueException("Please enter a Class Name");
+        }
+        if (acyear.isBlank()) {
+            throw new BlankValueException("Please enter an Academic Year");
+        }
+        String classCode = "";
+        List<ClassDto> classDtoList = classService.getAllClasses();
+        if (classDtoList.isEmpty()) {
+            throw new NotFoundException(("No classes found!"));
+        }
+        for (ClassDto classDto : classDtoList) {
+            if (classDto.getName().equals(className)) {
+                classCode = classDto.getCode();
+                break;
+            }
+        }
+        if (classCode.isBlank()) {
+            throw new InvalidValueException("Invalid Class Provided. Please enter a valid Class");
+        }
+        List<Schedule> scheduleList = scheduleService.getScheduleByClass(classCode);
+        List<Results> classResultList = null;
+        for (Schedule sc : scheduleList) {
+            if (sc.getYear().equals(acyear)) {
+                String schedule = sc.getScheduleCode();
+                classResultList.addAll(resultsRepository.findALlByscheduleCode(schedule));
+            }
+
+        }
+        if (classResultList.isEmpty()) {
+            throw new NotFoundException("Results not Found");
+        }
+    return classResultList;
+    }
+
 
     public Results addNewResult(Results result) {
         Student student = studentRepository.findById(result.getStudentId()).orElse(null);
@@ -58,4 +103,12 @@ public class ResultsService{
         return Optional.of(r);
     }
 
+    public void deleteResult(String resultCode) {
+        Results result = resultsRepository.findById(resultCode).orElse(null);
+        if(result == null){
+            throw new NotFoundException("Deletion Failed. No such Result!");
+
+        }
+        resultsRepository.delete(result);
+    }
 }
